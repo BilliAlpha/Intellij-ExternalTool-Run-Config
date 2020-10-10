6f02b630696d14544582c9acdec925dc65e86885
GitHub Action <action@github.com>
2020-10-10 14:43:38 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "Intellij-ExternalTool-Run-Config"
---
+++ src/main/kotlin/com/github/billialpha/intellijexternaltoolrunconfig/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.billialpha.intellijexternaltoolrunconfig.services
+
+import com.github.billialpha.intellijexternaltoolrunconfig.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
